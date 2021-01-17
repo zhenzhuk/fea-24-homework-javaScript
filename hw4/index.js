2f1af4a15db676113bad79057993b7ca55827da8
Yevheniia <gukovskayajane@gmail.com>
2021-01-17 20:19:42 +0200

    hw4 second commit

--- hw4/index.js
+++ hw4/index.js
@@ -4,7 +4,7 @@ const studentArr = [{
     ratingPoint: 1000,
     schoolPoint: 1000,
     course: 2,
-    },
+},
     {
         name: 'Татьяна',
         surname: 'Коваленко',
@@ -76,86 +76,3 @@ const studentArr = [{
         course: 2,
     },
 ];
-
-class Student{
-    constructor(enrole) {
-        Object.assign(this, enrole);
-        this.id = Student.id++;
-        this.sortUsersByRate();
-    }
-
-    static id = 1;
-
-    static studentList = [];
-
-
-    sortUsersByRate() {
-        Student.studentList.push(this);
-        Student.studentList.sort((a,b) => {
-            if (a.ratingPoint > b.ratingPoint) return -1;
-            if (a.ratingPoint < b.ratingPoint) return 1;
-            if (a.schoolPoint > b.schoolPoint) return -1;
-            if (a.schoolPoint < b.schoolPoint) return 1;
-            return 0;
-        })
-        for (let index in Student.studentList) {
-            const student = Student.studentList[index]
-            if (student.ratingPoint < 800) {
-                student.isSelfPayment = true;
-                continue;
-            }
-            student.isSelfPayment = index >= 5;
-        }
-    }
-
-    static budgetStudents(){
-        const result = [];
-        for (let student of this.studentList) {
-            !student.isSelfPayment && result.push(student)
-        }
-        return result;
-    }
-
-    static contractStudents(){
-        const result = [];
-        for (let student of this.studentList) {
-            student.isSelfPayment && result.push(student)
-        }
-        return result;
-    }
-
-}
-
-
-
-const newArr = studentArr.map(item => new Student(item))
-
-
-console.log('________________Student list____________________')
-console.log(Student.studentList)
-
-
-console.log('___________________budget_______________________')
-console.log(Student.budgetStudents())
-
-
-console.log('__________________contract______________________')
-console.log(Student.contractStudents())
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
